8b689503778ff2b898534d652aafd1353d15cdcb
KayaGombu <164357722+KayaGombu@users.noreply.github.com>
2024-04-25 10:10:21 -0400

    Update Main.java

--- AntarticaGUI/src/Main.java
+++ AntarticaGUI/src/Main.java
@@ -1,3 +1,7 @@
+/*
+ * 
+ * 
+ */
 package CongoBasin;
 import java.sql.*;
 import java.util.*;
@@ -20,46 +24,52 @@ public class Main {
       String temp[] = readFile.nextLine().split(",");
       users.add(new User(temp[0], temp[1], temp[1]));
     }
+    readFile.close();
     //Put sql connections/call methods here
     
     //add user's index to map
     for(int i = 0; i < users.size(); i++){
       userLookup.put(users.get(i).getUserData()[0], i);
     }
-    System.out.println("Hello world");
-    //Register cycle (Kaya)
     
   }
-  //Kaya
+  /**
+   * Checks whether the users login is valid
+   * @param name The user's username
+   * @param pass The user's password
+   * @return A boolean determining whether the login is valid
+   * By Kaya
+   */
   public static boolean login (String name, String pass){
-    /*if (userLookup.containsKey(name)){
+    if (userLookup.containsKey(name)){
       if (users.get(userLookup.get(name)).checkPass(pass)){
-        return true;
-      }
-    }
-    return false;*/
-    if (name.equals("User1")){
-      System.out.println("Test1");
-      if (Integer.toString("Password123".hashCode()).equals(Integer.toString(pass.hashCode()))){
-        System.out.println("Test2");
         return true;
       }
     }
     return false;
   }
-  //Kaya
-  public static void signUp (String name, String pass){
-    do{
 
-      if (userLookup.containsValue(name)){
+  /**
+   * Registers a new User
+   * @param name The inputted username
+   * @param pass the inputed password
+   * By Kaya
+   */
+  public static void signUp (String name, String pass) throws IOException{
+      if (userLookup.containsKey(name)){
         System.out.println("That username already exists");
       }
       else{
         User temp = new User(name, pass, "false");
-    users.add(temp);
-    userLookup.put(name, users.size());
+        users.add(temp);
+        userLookup.put(name, users.size());
+
+        FileWriter fw = new FileWriter("UserInfo.csv", true);
+        PrintWriter writeFile = new PrintWriter(fw);
+
+        writeFile.println(name + "," + pass + ",false");
+        writeFile.close();
       }
-    }while(userLookup.containsKey(name));
     
   }
   
