1da433b2a9ecd048984339ebcbcdccfedddc4d63
Potato21W <164357168+Potato21W@users.noreply.github.com>
2024-04-30 11:26:45 -0400

    Update Main.java

--- AntarticaGUI/src/CongoBasin/Main.java
+++ AntarticaGUI/src/CongoBasin/Main.java
@@ -1,80 +1,77 @@
-/*
- * 
- * 
- */
-package CongoBasin;
+package antarticagui;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
 import java.util.*;
-import java.io.*;
 public class Main {
   
   static ArrayList<Book> books = new ArrayList<Book>();
   static ArrayList<String> bookData = new ArrayList<String>();
+  
   static ArrayList<User> users = new ArrayList<User>();
   static Map<String, Integer> userLookup = new HashMap<String, Integer>();// Username, index in Users
   
-
-  public static void main(String[] args) throws IOException{
-
-    File myFile = new File("UserInfo.csv");
-    Scanner readFile = new Scanner(myFile);
-
-    //Read in all the usernames, passwords, and admin statuses
-    while (readFile.hasNext()){
-      String temp[] = readFile.nextLine().split(",");
-      users.add(new User(temp[0], temp[1], temp[1]));
-    }
-    readFile.close();
+  
+  public static void main(String[] args) {
     //Put sql connections/call methods here
     
     //add user's index to map
     for(int i = 0; i < users.size(); i++){
       userLookup.put(users.get(i).getUserData()[0], i);
     }
-    
-  }
-  /**
-   * Checks whether the users login is valid
-   * @param name The user's username
-   * @param pass The user's password
-   * @return A boolean determining whether the login is valid
-   * By Kaya
-   */
-  public static boolean login (String name, String pass){
-    if (userLookup.containsKey(name)){
-      if (users.get(userLookup.get(name)).checkPass(pass)){
-        return true;
-      }
-    }
-    return false;
   }
 
-  /**
-   * Registers a new User
-   * @param name The inputted username
-   * @param pass the inputed password
-   * By Kaya
-   */
-  public static void signUp (String name, String pass) throws IOException{
-      if (userLookup.containsKey(name)){
+    //Register cycle (Kaya)
+    /*do{
+      //Get from GUI
+      String user = ;
+      String pass;
+
+      if (userLookup.containsValue(user)) {
         System.out.println("That username already exists");
       }
       else{
-        User temp = new User(name, pass, "false");
-        users.add(temp);
-        userLookup.put(name, users.size());
-
-        FileWriter fw = new FileWriter("UserInfo.csv", true);
-        PrintWriter writeFile = new PrintWriter(fw);
-
-        writeFile.println(name + "," + pass + ",false");
-        writeFile.close();
+        signUp(user, pass);
       }
-    
-  }
+    }while(userLookup.containsKey(users));
+  }*/
+  
+  //Kaya
+  public static boolean login (String name, String pass){
+	  
+	    if (userLookup.containsKey(name)){
+	      if (users.get(userLookup.get(name)).checkPass(pass)) {
+	        return true;
+	      }
+	    }
+	    return false;
+	  }
+  
+	  //Kaya
+	  @SuppressWarnings({ "unlikely-arg-type" })
+	  public static void signUp (String name, String pass) throws IOException{
+	      if (userLookup.containsKey(name)){
+	        System.out.println("That username already exists");
+	      }
+	      else{
+	        User temp = new User(name, pass, "false");
+	        users.add(temp);
+	        userLookup.put(name, users.size());
 
-  public static void getBook() throws IOException{
-		  File file = new File("BookData");
+	        FileWriter fw = new FileWriter("UserInfo.csv", true);
+	        PrintWriter writeFile = new PrintWriter(fw);
+
+	        writeFile.println(name + "," + pass + ",false");
+	        writeFile.close();
+	      }
+	    
+	  }
+	  
+	  public static void getBook() throws IOException{
+		  File file = new File("BookData.csv");
 		  
 		  Scanner inputFile = new Scanner(file);
 		  int counter = 0;
@@ -94,6 +91,29 @@ public class Main {
 		  
 		  inputFile.close();
 		 
+	  }
+	  
+	  public static void toMap(String name, String pass) {
+		  User temp = new User(name, pass, "false");
+	        users.add(temp);
+	        userLookup.put(name, users.size());
+	  }
+	  
+	  public static void checkMap() throws IOException{
+		  File file = new File("UserInfo.csv");
+		  Scanner fileIn = new Scanner(file);
+		  String[] in;
+		  String line;
+		  
+		  while (fileIn.hasNext()) {
+			  line = fileIn.nextLine();
+			  System.out.println(line);
+			  in = line.split(",");
+			  toMap(in[0],in[1]);
+			  
+		  }
+		  
+		  fileIn.close();
 	  }
   
 }
